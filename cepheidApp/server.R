5046f7966dfe0b19139dc620715df62097117166
origamiwolf <origamiwolf@gmail.com>
2015-10-15 04:40:07 +0000

    Polished version 1

--- cepheidApp/server.R
+++ cepheidApp/server.R
@@ -14,33 +14,54 @@ shinyServer(function(input,output) {
     processCephData(cleanCeph,input$smcDist)
   })
 
-  cephFitsApp <- reactive({
-    fitCephData(cleanCeph,"appMag")    
-  })
+#   Not needed for now
+#   cephFitsApp <- reactive({
+#     fitCephData(cleanCeph,"appMag")    
+#   })
     
   output$plot <- renderPlot({
     plotData<-cephData()
 
+    # setup the label vectors
+    plotLabels<-list(
+                  title = "Cepheid Period-Magnitude Plot",
+                  x = "Period (days)",
+                  y = "Apparent Magnitude",
+                  color = "Magnitude"
+                )
+    
     # determine if plot is linear or log    
     if (input$plotAxis=="linear") {
       xPlot<-"Period"
       smoothForm<-"y~log10(x)"
+      plotLabels$x<-"Period (days)"
     } else {
       xPlot<-"log10(Period)"
       smoothForm<-"y~x"
+      plotLabels$x<-"Log(Period/1 day)"
     }
 
     # determine if plot is for absolute or apparent magnitude
     if (input$magType=="appMag") {
       yPlot<-"appMag"
+      plotLabels$y<-"Apparent Magnitude"
     } else {
       yPlot<-"absMag"
+      plotLabels$y<-"Absolute Magnitude"
     }
-    
+
+    # plot according to selected inputs    
     p <- ggplot(plotData,aes_string(xPlot,yPlot,col="magVal"))
-    p <- p + geom_point()
+    p <- p + geom_point(size=3)
+    
+    # stellar magnitudes are traditionally shown in reverse order
     p <- p + scale_y_reverse()
 
+    # add labels and use better colours
+    p <- p + labs(plotLabels) + scale_colour_manual(
+                                  values=c("#0072B2", "#D55E00")
+                                )
+    
     # determine whether to show regression lines
     if (input$showModels == TRUE) {
       p <- p+stat_smooth(method="lm", formula = smoothForm)
--- cepheidApp/ui.R
+++ cepheidApp/ui.R
@@ -4,25 +4,21 @@ shinyUI(fluidPage(
   titlePanel("Cepheid Period-Luminosity Relationship"),
   sidebarLayout(
     sidebarPanel(
-      h2("Widgets go here"),
       sliderInput("smcDist", "Distance to Small Magellanic Cloud (in units of kilo light years)",
                   min = 190,
                   max = 210,
                   value = 200,
                   step = 0.5),
       br(),
-      br(),
       radioButtons("plotAxis","Select axis type:",
                    c("Linear" = "linear",
                      "Logarithmic" = "log")
         ),
       br(),
-      br(),
       radioButtons("magType","Display:",
                    c("Apparent Magnitude" = "appMag",
                      "Absolute Magnitude" = "absMag")
       ),
-      br(),
       br(),
       checkboxInput("showModels","Display regression lines?")
     ),
